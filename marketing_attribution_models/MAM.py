dd9f0c389465511a79a837123691353c046d10aa
Tommaso Di Marcello <tommaso.dimarcello@which.co.uk>
2025-02-13 14:42:26 +0000

    changes to seaborn style and df_temp['time_till_conv']

--- marketing_attribution_models/MAM.py
+++ marketing_attribution_models/MAM.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-plt.style.use("seaborn-white")
+
 
 
 class MAM:
@@ -267,7 +267,7 @@ class MAM:
             df_temp["time_till_conv"] = (
                 df_temp[group_timestamp_colname + "_y"]
                 - df_temp[group_timestamp_colname + "_x"]
-            ).astype("timedelta64[h]")
+            ).dt.total_seconds() / 3600
 
             df_temp = (
                 df_temp.groupby(group_channels_by_id_list)["time_till_conv"]
